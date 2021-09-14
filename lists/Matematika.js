a31eb9d896ae8e4d6af3b3d6994194abd692d68b
Schmidt Péter <schmidt.peti32@gmail.com>
2021-09-14 12:56:30 +0200

    Making new dictionary lists

--- lists/Matematika.js
+++ lists/Matematika.js
@@ -12,7 +12,7 @@ const Matematika_alapok_1 = [
     {topic: "Matematikai alapok 1. zárthelyi"},
     {topic: "Matematikai alapok 2. zárthelyi"},
     {topic: "Matematikai alapok 3. zárthelyi"},
-    {topic: "Matematikai alapok 4. zárthelyi"},
+    {topic: "Matematikai alapok 4. zárthelyi"}
 ];
 const Matematika_alapok_2 = [
     {topic: "Komplex számok"},
@@ -41,4 +41,42 @@ const Matematika_alapok_3 = [
     {topic: "Matematikai alapok 3 2. zárthelyi"},
     {topic: "Matematikai alapok 3 3. zárthelyi"}
 ];
-module.exports = {Matematika_alapok_1,Matematika_alapok_2,Matematika_alapok_3};
+const Anal_1 = [
+    {topic: "Analízis Valós Számok Struktúrája"},
+    {topic: "Valós Számok Struktúrája"},
+    {topic: "Analízis egyenlőtlenségek"},
+    {topic: "Analízis Számhalmaz szuprémuma és infimuma"},
+    {topic: "Függvények"},
+    {topic: "Valós sorozatok 1."},
+    {topic: "Valós sorozatok 2."},
+    {topic: "Valós sorozatok 3."},
+    {topic: "Analízis 1 1. zárthelyi"},
+    {topic: "Analízis 1 2. zárthelyi"},
+    {topic: "Analízis 1 3. zárthelyi"},
+    {topic: "Analízis 1 4. zárthelyi"},
+    {topic: "Analízis 1 5. zárthelyi"}
+];
+const Anal_2 = [
+    {topic: "Végtelen sorok 1."},
+    {topic: "Végtelen sorok 2."},
+    {topic: "Végtelen sorok 3."},
+    {topic: "Függvények határértéke és folytonossága 1."},
+    {topic: "Függvények határértéke és folytonossága 2."},
+    {topic: "Függvények határértéke és folytonossága 3."},
+    {topic: "Anal Vizsga"}
+]
+const DiMat1 = [
+    {topic: "Binomiális/polinomiális tétel, szita formula, skatulya-elv"},
+    {topic: "Komplex számok algebrai és trigonometrikus alakja"},
+    {topic: "Gráfok alapfogalmai, utak, séták, fák"},
+    {topic: "Gráfok alapfogalmai 1."},
+    {topic: "Gráfok alapfogalmai 2."},
+    {topic: "Kombinatorika 2."},
+    {topic: "Komplex számok algebrai és trigonometrikus alakja + Szürjekció"},
+    {topic: "Kombinatorika"},
+    {topic: "Összetett leszámlálási feladatok"},
+    {topic: "Függvények, részbenrendezés"},
+    {topic: "Relációk tulajdonságai, osztályfelbontás, ekvivalenciareláció"},
+    {topic: "Diszkrét matematika halmazok"},
+]
+module.exports = {Matematika_alapok_1,Matematika_alapok_2,Matematika_alapok_3,Anal_1,Anal_2,DiMat1};
--- lists/listDictionary.js
+++ lists/listDictionary.js
@@ -1,7 +1,11 @@
-const {Matematika_alapok_1,Matematika_alapok_2} = require('../lists/Matematika');
+const {Matematika_alapok_1,Matematika_alapok_2,Matematika_alapok_3,Anal_1,Anal_2,DiMat1} = require('../lists/Matematika');
 
 const Categories = [
     Matematika_alapok_1,
-    Matematika_alapok_2
+    Matematika_alapok_2,
+    Matematika_alapok_3,
+    Anal_1,
+    Anal_2,
+    DiMat1
 ]
 module.exports = Categories;
